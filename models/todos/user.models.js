b618f370ebc36f0d12bb14c282f624e85762971b
anikettikkal <tikkalaniket2019@gmail.com>
2025-11-25 21:12:48 +0530

    models can be created using mongoose

---
+++ models/todos/user.models.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true
+        },
+        password: {
+            type: String,
+            required: true
+        }
+    } , {timestamps: true} // timestamps will automatically add createdAt and updatedAt fields
+)
+
+export const User = mongoose.model("User", userSchema);
